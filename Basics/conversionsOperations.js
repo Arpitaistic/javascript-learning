2d2af21a942d58a8326608790cd2df2c37050e5c
Arpita <arpitaistic@gmail.com>
2025-07-27 14:14:18 +0530

    add conversionsOperations

---
+++ Basics/conversionsOperations.js
@@ -0,0 +1,24 @@
+//let score = "30";
+let score = null;
+console.log(typeof score);
+console.log (typeof(score));
+
+let valueInNumber = Number(score);
+console.log (typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = 1;
+let isLoggedInBoolean = Boolean (isLoggedIn);
+console.log (isLoggedInBoolean);
+// 1 => true;
+// 0 => false;
+// "" => false;
+// "Arpita" => true;
+let someNumber = 33;
+let someNumberString = String (someNumber);
+console.log (someNumberString);
+console.log (typeof someNumberString); //string
