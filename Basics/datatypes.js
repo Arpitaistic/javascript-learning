f53e0deaf3408b4e4ba3229ae7618b31393735a7
Arpita <arpitaistic@gmail.com>
2025-07-27 13:52:27 +0530

    add variables and datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+console.log(3+
+    3
+) // not ideal for code readannility, but valid JS
+console.log("Arpita");
+let name = "Arpita";
+let age = 22;
+let isLoggedIn = true;
+let state = null; //no value
+console.log (typeof "Arpita");
+//primitive data types
+//number => 2 to the power of 53
+//bigInt 
+//string ""
+//boolean => true/false
+//null => empty value
+//undefined => variable not defined
+//symbol => unique value
+
+//Objects
+console.log (typeof null); //object, this is a bug in JS
+console.log (typeof undefined); //undefined
