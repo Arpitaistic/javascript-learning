1d4a5aa2bf76721bd50618e19034fd24b1a316fd
Arpita <arpitaistic@gmail.com>
2025-08-07 23:37:22 +0530

    Add Objects in JS

---
+++ Basics_02/Objects_02.js
@@ -0,0 +1,14 @@
+//Objects
+//Objects literals
+
+const JsUser = {
+    name: "Arpita",
+    age:  21,
+    location:'India',
+    email: 'arpitaistic@gmail.com',
+    isLoggedIn: true,
+    lastLoggedInDays: ["Monday", "Tuesday", "Wednesday"],
+}
+console.log(JsUser.email);
+
+//Creating Symbols in JS.
