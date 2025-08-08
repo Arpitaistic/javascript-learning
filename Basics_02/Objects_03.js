4e18b24ad645b1fbdd8679ee2f380385a82f48cb
Arpita <arpitaistic@gmail.com>
2025-08-08 21:20:00 +0530

    add objects_03

---
+++ Basics_02/Objects_03.js
@@ -0,0 +1,39 @@
+//const tinderUser = new Object();
+const tinderUser = {}
+tinderUser.name = "Klaus";
+tinderUser.id = "abc124";
+tinderUser.isLoggedIn = false;
+//console.log (tinderUser);
+
+const regularUser ={
+    email: "elena123gilbert@gmail.com",
+    fullname : {
+        userFullname :{
+           firstName: "Elena",
+           lastName: "Gilbert",
+        }
+    }
+}
+console.log (regularUser);
+
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "a", 4: "b"};
+//const obj3 = Object.assign({}, obj1, obj2);
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+const users =[
+    {
+    name: "Arpita",
+    email: "arpitaistic@gmail.com"
+    },
+    {
+
+    },
+]
+users[1].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
