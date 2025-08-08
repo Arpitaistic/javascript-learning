c4f414e61487ad6a32dcf5c8bd52728fb280a7f4
Arpita <arpitaistic@gmail.com>
2025-08-08 20:51:39 +0530

    add symbols in JS

--- Basics_02/Objects_02.js
+++ Basics_02/Objects_02.js
@@ -12,3 +12,12 @@ const JsUser = {
 console.log(JsUser.email);
 
 //Creating Symbols in JS.
+const mySym = Symbol("mykey1");
+// If you want to add the symbol as a property to JsUser:
+JsUser[mySym] = "mykey1";
+//Object.freeze(JsUser); // This will prevent any changes to the JSUser Object.
+
+JsUser.greeting = function(){
+    console.log(`Hello, my name is ${this.name}`);
+}
+console.log(JsUser.greeting());
