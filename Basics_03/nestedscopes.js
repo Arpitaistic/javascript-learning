7e75c5f7300554e4c5cb2f0b8b154b56f64354e6
Arpita <arpitaistic@gmail.com>
2025-08-11 10:24:41 +0530

    add nestedscope in JS

---
+++ Basics_03/nestedscopes.js
@@ -0,0 +1,36 @@
+function one (){
+    const username = "Arpita"
+    function two (){
+        const website = "Youtube"
+       // console.log(username)
+    }
+    //console.log(website)
+    //two()
+}
+//one()
+
+
+if (true){
+   const userName = "Klaus"
+   if (userName == "Klaus"){
+    const website = "Netfiix"
+    //console.log(userName + website)
+   }
+   //console.log(website)
+}
+//console.log(userName)
+
+// ++++++++++ INTERESTING +++++++//
+addone(6)
+function addone(num){
+       return num+1
+     
+}
+  console.log(addone)
+
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(6)
