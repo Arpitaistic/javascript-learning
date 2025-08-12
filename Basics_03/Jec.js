c133f82e9991a51ce848f4388d30fdbc0032f2d7
Arpita <arpitaistic@gmail.com>
2025-08-12 11:43:24 +0530

    Add Java Execution Code

---
+++ Basics_03/Jec.js
@@ -0,0 +1,14 @@
+function greet() {
+  console.log("Hello");
+}
+
+function sayName(name) {
+  console.log("My name is " + name);
+}
+
+function start() {
+  greet();
+  sayName("Arpita");
+}
+
+start();
