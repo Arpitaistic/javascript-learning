ee7ec5f4e7f73741c49a10d52cf6f16f9674aafb
Arpita <arpitaistic@gmail.com>
2025-08-15 00:21:16 +0530

    add conditionals

---
+++ ControlFlow_04/conditionals.js
@@ -0,0 +1,31 @@
+const isUserloggedIn = true
+if (2 === "2"){
+    // console.log("Executed");
+}
+// <, >, <=, >=, ==, ===, !=, !==
+
+const temperature = 51;
+if (temperature <= 50){
+    //console.log("Temperature is still hot!")
+} else{
+    //console.log ("Temperature is hot!")
+}
+
+//console.log("What's the temperature at your place?")
+
+//Short-hand Notation
+const balance = 1000
+//if (balance > 500) console.log ("Insufficient Balance!"), //Immature code
+//console.log("Test 2");
+/*if (balance < 500){
+    console.log("less than 500");
+} else if (balance < 750){
+    console.log("less than 750");
+} else if (balance < 900){
+    console.log("less than 1200");
+}*/
+const userLoggedIn = true
+const userDebitCard = true
+if (userLoggedIn && userDebitCard == true){
+    console.log("Allow to buy course")
+}
