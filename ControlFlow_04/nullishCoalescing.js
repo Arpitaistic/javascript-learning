315a98a6f9508ce5003cd14220151664e05b77bc
Arpita <arpitaistic@gmail.com>
2025-08-15 22:09:26 +0530

    added Terniary Operator

--- ControlFlow_04/nullishCoalescing.js
+++ ControlFlow_04/nullishCoalescing.js
@@ -3,4 +3,8 @@ let val1;
 //val1 = 5 ?? 10
 //null=null ?? 10; // If val1 is null, it will take the value 10
 val1 = undefined ?? 10; // If val1 is undefined, it will take the value 10
-console.log(val1);
+//sole.log(val1);
+
+//Terniary Operator: (condition) ? valueIfTrue : valueIfFalse
+const iceTeaPrice = 50;
+iceTeaPrice < 50 ? console.log("Ice Tea is affordable") : console.log("Ice Tea is expensive");
