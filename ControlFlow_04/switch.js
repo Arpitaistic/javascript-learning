e693572e9b8883b4436ecadc1abcbc7d6ac12e23
Arpita <arpitaistic@gmail.com>
2025-08-15 21:23:56 +0530

    Switch in control Flow

---
+++ ControlFlow_04/switch.js
@@ -0,0 +1,24 @@
+//SWITCH IN JS
+//Syntax:
+// switch (key){
+// case value1:
+
+// break;
+// default:
+//break;}
+ const month = 2
+ switch (month) {
+    case 1:
+        console.log("January");
+         break;
+    case 2:
+        console.log ("February");
+        break;
+    case 3:
+        console.log ("March");
+        break;
+        default:
+            console.log("Invalid Month");
+            break;
+    }
+   
