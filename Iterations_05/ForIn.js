8f62eb8a9fa15bc42ba0ba5b95ce0c86d877d57b
Arpita <arpitaistic@gmail.com>
2025-08-17 15:58:14 +0530

    For In with Objects and Arrays

---
+++ Iterations_05/ForIn.js
@@ -0,0 +1,16 @@
+const myObject = {
+    js : 'JavaScript',
+    py : 'Python',
+    Java : 'Java',
+    cpp : 'C++',
+}
+//For in with Objects
+for ( const key in myObject){
+    console.log (myObject[key]);
+}
+
+//For in with Arrays
+const programmingLanguages = ['JavaScript', 'Python', 'Java', 'c++'];
+for ( const key in programmingLanguages){
+    console.log(keys);
+}
