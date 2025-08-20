28ffd9eaadf1d75ea8883b11541b9eae6e4d9440
Arpita <arpitaistic@gmail.com>
2025-08-20 15:59:20 +0530

    examples in reduce

--- FilterMapandReduce.js/reduce.js
+++ FilterMapandReduce.js/reduce.js
@@ -4,3 +4,18 @@ const myTotal = myNums.reduce ((accumulator, currentValue) =>{
     return accumulator + currentValue;
 }, 0)
 console.log (`Total: ${myTotal}`);
+
+//Example 2
+const shoppingCart = [
+    {
+        item: 'Apple', price: 10, quantity: 2
+    },
+    {
+        item: 'Banana', price: 5, quantity: 5
+    },
+    {
+        item: 'Orange', price: 8, quantity: 3
+    }
+]
+const priceToPay = shoppingCart.reduce((accumulator, item)=>accumulator + item.price, 0);
+console.log(`Total Price to Pay: ${priceToPay}`);
